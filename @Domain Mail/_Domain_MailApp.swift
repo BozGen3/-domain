0b54fc6d6bf30e268503cf953897a67d7945d80d
Bozhidar Genchev <bozhidargenchev@abv.bg>
2024-12-01 17:15:31 +0200

    Initial Commit

---
+++ @Domain Mail/_Domain_MailApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Domain_MailApp.swift
+//  @Domain Mail
+//
+//  Created by Bozhidar Genchev on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct _Domain_MailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ @Domain MailTests/_Domain_MailTests.swift
@@ -0,0 +1,16 @@
+//
+//  _Domain_MailTests.swift
+//  @Domain MailTests
+//
+//  Created by Bozhidar Genchev on 01/12/2024.
+//
+
+import Testing
+
+struct _Domain_MailTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
